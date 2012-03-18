a1623a6420df9b5e4579640d7e1e935a229a4c3c
unknown <abebe@.(none)>
2012-03-18 12:47:39 +0900

    seed User

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,3 +43,8 @@ Product.create(title: 'Rails Test Prescriptions',
       </p>},
   image_url: 'rtp.jpg',
   price: 34.95)
+
+User.delete_all
+User.create(name: 'abkdepot',
+  password: 'abkdepot',
+  password_confirmation: 'abkdepot')
